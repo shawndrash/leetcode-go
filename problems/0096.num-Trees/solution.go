391327b89a458702e9b4038e3c38b77cf3b68420
shawndrash <shawnidrash@gmail.com>
2026-03-11 17:55:04 +0800

    exercise: 动态规划

---
+++ problems/0096.num-Trees/solution.go
@@ -0,0 +1,28 @@
+package num_Trees
+
+// 如果是链表题目，取消下面的注释：
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
+
+// 如果是树题目，取消下面的注释：
+// type TreeNode struct {
+// 	Val   int
+// 	Left  *TreeNode
+// 	Right *TreeNode
+// }
+
+// ============= 以下是提交到 LeetCode 的代码 =============
+func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+
+	return dp[n]
+}
