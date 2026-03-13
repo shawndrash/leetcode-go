70d879f988399454d580152eb1f0d916409fb7b5
shawndrash <shawnidrash@gmail.com>
2026-03-13 17:34:31 +0800

    exercise: 动态规划（背包）

--- problems/0279.num-squares/solution.go
+++ problems/0279.num-squares/solution.go
@@ -15,23 +15,15 @@ package num_squares
 
 // ============= 以下是提交到 LeetCode 的代码 =============
 func numSquares(n int) int {
-	maxNum, index := 0, 0
-	for index*index <= n {
-		maxNum = index
-		index++
-	}
-	nums, dp := make([]int, maxNum), make([]int, n+1)
+	dp := make([]int, n+1)
 	dp[0] = 0
-	for i := range nums {
-		nums[i] = i + 1
-	}
 	for i := 1; i < len(dp); i++ {
 		dp[i] = 10001
 	}
 
-	for i := 0; i < len(nums); i++ {
-		for j := nums[i] * nums[i]; j <= n; j++ {
-			dp[j] = min(dp[j], dp[j-nums[i]*nums[i]]+1)
+	for i := 0; i*i <= n; i++ {
+		for j := i * i; j <= n; j++ {
+			dp[j] = min(dp[j], dp[j-i*i]+1)
 		}
 	}
 
