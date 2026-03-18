7ef42b79ecb7a34db0678ffe856b72d330cef3af
shawndrash <shawnidrash@gmail.com>
2026-03-18 10:55:55 +0800

    exercise: 动态规划（买卖股票）

---
+++ problems/0714.max-profit-fee/solution.go
@@ -0,0 +1,26 @@
+package max_profit_fee
+
+// 如果是链表题目，取消下面的注释：
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
+
+// 如果是树题目，取消下面的注释：
+// type TreeNode struct {
+// 	Val   int
+// 	Left  *TreeNode
+// 	Right *TreeNode
+// }
+
+// ============= 以下是提交到 LeetCode 的代码 =============
+func maxProfit(prices []int, fee int) int {
+	dp := make([][2]int, len(prices))
+	dp[0][0] = -prices[0]
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i])
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i]-fee)
+	}
+
+	return dp[len(dp)-1][1]
+}
